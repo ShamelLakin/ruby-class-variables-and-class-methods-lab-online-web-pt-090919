4a6525186fac52338e3dd4e748d6ca296521c51b
Shamel Lakin <christlife4u@gmail.com>
2019-09-30 17:22:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,9 +40,9 @@ class Song
    genre_count
  end 
  
- def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
+ def self.artist_count
+    artist_count = {}
+    @@artists.each do |genre|
      if genre_count[genre] 
        genre_count[genre] += 1
        else 
