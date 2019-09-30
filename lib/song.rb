dc6b80e67c88e52887b1c3c4aeda124c97980798
Shamel Lakin <christlife4u@gmail.com>
2019-09-30 17:25:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,7 @@ class Song
  def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-     if artist_count[artist] 
-       artist_count[artist] += 1
+     if artist_count[artist] += 1
        else 
         artist_count[artist] = 1
      end
